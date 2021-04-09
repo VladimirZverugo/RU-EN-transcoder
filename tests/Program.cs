df8eb51cf449d3933ec5a680ff59f2229cc2ac82
Vladimir <44143087+VladimirZverugo@users.noreply.github.com>
2021-04-09 11:48:35 +0300

    feat: upload task files

---
+++ tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DEV3
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseConverter baseConverter = new BaseConverter();
+            string converted = baseConverter.Convert(args);
+            Console.WriteLine(converted);
+
+            Console.Read();
+        }
+    }
+}
